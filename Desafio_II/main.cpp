886187334b50af5cd34d8639ac26a37043eb1754
ElDiego2101 <eldiegoalex.parada@gmail.com>
2024-10-15 04:23:37 -0500

    Update main.cpp
    Se agregan funcionalidades para más opciones del main

--- Desafio_II/main.cpp
+++ Desafio_II/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include "red.h"
 #include "complementario.h"
+#include "venta.h"
 using namespace std;
 
 
 int main() {
-    char opcion;
-    bool ban = true;
-    Red TerMex;
+    string nombreArchivo = "baseDatos";
+    short dia = leerDia(nombreArchivo);
     string hora=horaReal();
+    bool ver=true;
+    bool ban = true;
+    char opcion;
+    Red TerMex;
+    cout << "Dia actual: " << dia << endl;
     cout<<hora<<endl;
     cout << "**Bienvenido a la red de combustible 'TerMax'.**" << endl << endl;
     while (ban) {
@@ -19,10 +24,10 @@ int main() {
              << "4. Agregar un surtidor a una (E/S)." << endl
              << "5. Eliminar un surtidor de una (E/S)." << endl
              << "6. Consultar las transacciones de los surtidores de una (E/S)." << endl
-             << "7. Activar un surtidor de una (E/S)." << endl
-             << "8. Desactivar un surtidor de una (E/S)." << endl
-             << "9. Cantidad de litros vendidos para cada categoria de combustible." << endl
-             << "M. Simulacion de una venta de combustible." << endl
+             << "7. Cambiar estado de un surtidor." << endl
+             << "8. Cantidad de litros vendidos para cada categoria de combustible." << endl
+             << "9. Simulacion de una venta de combustible." << endl
+             <<"M. Ajustar precios del combustible."<<endl
              << "N. Salir." << endl << endl
 
              << "Opcion: ";
@@ -45,8 +50,12 @@ int main() {
             TerMex.AgregarSurtidor();
             break;
         case '5':
+            limpiarConsola();
+            TerMex.ELiminarSurtidor();
             break;
         case '6':
+            limpiarConsola();
+            TerMex.ConsultarTransacciones();
             break;
         case '7':
             limpiarConsola();
@@ -54,15 +63,27 @@ int main() {
             break;
         case '8':
             break;
-        case '9':
+        case '9'://marco el ambito de esas variables
+        limpiarConsola();
+        TerMex.SimulacionVenta();
             break;
         case 'M':
+            if(ver){
+                TerMex.AjustarPrecios();
+                ver=false;
+            }else{
+                cout<<"lo sentimos, espera al siguiente dia para ajustar los precios"<<endl;
+            }
             break;
         case 'N':
+
+            actualizarDia(nombreArchivo, dia);
+            ban=false;
             break;
         case 'J':
             limpiarConsola();
-           TerMex.consulta();
+            //TerMex.consulta();
+            TerMex.SimulacionVenta();
             break;
         default:
             cout<<"elija una opcion valida"<<endl;
