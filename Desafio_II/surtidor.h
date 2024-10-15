40916b6259ea73005f57fcbccd15e88720a925e6
ElDiego2101 <eldiegoalex.parada@gmail.com>
2024-10-15 04:16:32 -0500

    Update surtidor.h
    Se agregan funciones para los estados de los surtidores, para las ventas y para el ID de los surtidores

--- Desafio_II/surtidor.h
+++ Desafio_II/surtidor.h
@@ -1,6 +1,7 @@
 #ifndef SURTIDOR_H
 #define SURTIDOR_H
 #include <iostream>
+#include "venta.h"
 using namespace std;
 
 class Surtidor
@@ -9,10 +10,14 @@ private:
     string ID_sr;
     string Model_mq;
     bool estado_surtidor;
+    Venta** ventas;
+    int num_ventas;
 public:
     Surtidor(string _ID_sr, string _Model_mq, bool _estado_surtidor);
+    string getID_sr() const;
     string getNombre();
-
+    void AgregarVenta(Venta* nuevaVenta);
+    void MostrarVentas() const;
     bool getEstado_surtidor() const;
     void setEstado_surtidor(bool newEstado_surtidor);
 };
