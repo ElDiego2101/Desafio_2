b12b5a95ec6a9aeddce2d82b9cb42dbb504fd8cb
ElDiego2101 <eldiegoalex.parada@gmail.com>
2024-10-15 04:14:24 -0500

    Update red.h
    Se agregan los precios de cada combustible y las funciones creadas para crear y eliminar estaciones y surtidores  en la red

--- Desafio_II/red.h
+++ Desafio_II/red.h
@@ -10,20 +10,30 @@ private:
     Estacion** arreglo_estacion;
     Surtidor*** arreglo_surtidores;
     Venta** arreglo_ventas;
+    short** precios;
     int* contador_surtidores;
     int pos_est;
     int capacidad;
     int capacidad_est;
+    short precioRegular;
+    short precioPremiun;
+    short precioEcoMax;
+    friend short randomNumero(int limite_i,int limite_s,string semilla);
+
 
 public:
     Red();
     ~Red();
     void AgregarEstacion();
+    void AjustarPrecios();
     void EliminarEstacion();
     void consulta();
     void AgregarSurtidor();
+    void ELiminarSurtidor();
     //vamos hacer que si mandamos 1 activamos y si mandamos cero desactivemos
     void EstadoSurtidor();
+    void SimulacionVenta();
+    void ConsultarTransacciones();
 
 
 };
